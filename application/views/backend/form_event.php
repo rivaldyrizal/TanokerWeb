c0474f45bb060ef2e1fc635a0c09ba410458493d
rivaldyrizal <rivaldyrizalul@gmail.com>
2019-05-11 16:35:47 +0700

    Beautify code at form_event

--- application/views/backend/form_event.php
+++ application/views/backend/form_event.php
@@ -1,115 +1,125 @@
 <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Event Tanoker</h3>
-              </div>
+	<div class="">
+		<div class="page-title">
+			<div class="title_left">
+				<h3>Event Tanoker</h3>
+			</div>
 
-              <div class="title_right">
-                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for...">
-                    <span class="input-group-btn">
-                              <button class="btn btn-default" type="button">Go!</button>
-                          </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="clearfix"></div>
+			<div class="title_right">
+				<div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search for...">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="button">Go!</button>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
 
-            <div class="row">
-              <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Form Penambahan Event <small>Wisata Tanoker</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<div class="x_panel">
+					<div class="x_title">
+						<h2>Form Penambahan Event <small>Wisata Tanoker</small></h2>
+						<ul class="nav navbar-right panel_toolbox">
+							<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
+										class="fa fa-wrench"></i></a>
+								<ul class="dropdown-menu" role="menu">
+									<li><a href="#">Settings 1</a>
+									</li>
+									<li><a href="#">Settings 2</a>
+									</li>
+								</ul>
+							</li>
+							<li><a class="close-link"><i class="fa fa-close"></i></a>
+							</li>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div class="x_content">
 
-                    <form action="<?php echo base_url('admin/event/tambah_aksi'); ?>" method="post" data-parsley-validate class="form-horizontal form-label-left">
+						<form action="<?php echo base_url('admin/event/tambah_aksi'); ?>" method="post" data-parsley-validate
+							class="form-horizontal form-label-left">
 
-                      
-                      <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Nama Event <span class="required">*</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <input id="name" class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" name="nama" required="required" type="text">
-                        </div>
-                      </div>
-                      <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="textarea">Deskripsi Event <span class="required">*</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                          <textarea id="textarea" required="required" name="konten" class="form-control col-md-7 col-xs-12"></textarea>
-                        </div>
-                      </div>
-              <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="textarea">Tanggal Mulai <span class="required">*</span>
-                        </label>
-                         <div class='col-sm-6'>
-                    <div class="form-group">
-                        <div class='input-group date' id='myDatepicker2'>
-                            <input type='text' class="form-control" name="tanggal_mulai" />
-                            <span class="input-group-addon">
-                               <span class="glyphicon glyphicon-calendar"></span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-              </div>
 
-              <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="textarea">Tanggal Selesai <span class="required">*</span>
-                        </label>
-                         <div class='col-sm-6'>
-                    <div class="form-group">
-                        <div class='input-group date' id='myDatepicker2'>
-                            <input type='text' class="form-control" name="tanggal_selesai" />
-                            <span class="input-group-addon">
-                               <span class="glyphicon glyphicon-calendar"></span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-              </div>
-                  <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Pilih Gambar <span class="required">*</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                        <div class="btn-group">
-                          <a class="btn" title="Insert picture (or just drag & drop)" id="pictureBtn"><i class="fa fa-picture-o"></i></a>
-                          <input type="file" data-role="magic-overlay" data-target="#pictureBtn" data-edit="insertImage" />
-                       </div>
-                     </div>
-                   </div>
-                      
-                      <div class="ln_solid"></div>
-                      <div class="form-group">
-                        <div class="col-md-6 col-md-offset-3">
-                          <!-- <button type="submit" class="btn btn-primary">Cancel</button> -->
-                          <button id="send" type="submit" class="btn btn-success">Input</button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+							<div class="item form-group">
+								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Nama Event <span
+										class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 col-xs-12">
+									<input id="name" class="form-control col-md-7 col-xs-12" data-validate-length-range="6"
+										data-validate-words="2" name="nama" required="required" type="text">
+								</div>
+							</div>
+							<div class="item form-group">
+								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="textarea">Deskripsi Event <span
+										class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 col-xs-12">
+									<textarea id="textarea" required="required" name="konten"
+										class="form-control col-md-7 col-xs-12"></textarea>
+								</div>
+							</div>
+							<div class="item form-group">
+								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="textarea">Tanggal Mulai <span
+										class="required">*</span>
+								</label>
+								<div class='col-sm-6'>
+									<div class="form-group">
+										<div class='input-group date' id='myDatepicker2'>
+											<input type='text' class="form-control" name="tanggal_mulai" />
+											<span class="input-group-addon">
+												<span class="glyphicon glyphicon-calendar"></span>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="item form-group">
+								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="textarea">Tanggal Selesai <span
+										class="required">*</span>
+								</label>
+								<div class='col-sm-6'>
+									<div class="form-group">
+										<div class='input-group date' id='myDatepicker2'>
+											<input type='text' class="form-control" name="tanggal_selesai" />
+											<span class="input-group-addon">
+												<span class="glyphicon glyphicon-calendar"></span>
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="item form-group">
+								<label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Pilih Gambar <span
+										class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 col-xs-12">
+									<div class="btn-group">
+										<a class="btn" title="Insert picture (or just drag & drop)" id="pictureBtn"><i
+												class="fa fa-picture-o"></i></a>
+										<input type="file" data-role="magic-overlay" data-target="#pictureBtn" data-edit="insertImage" />
+									</div>
+								</div>
+							</div>
+
+							<div class="ln_solid"></div>
+							<div class="form-group">
+								<div class="col-md-6 col-md-offset-3">
+									<!-- <button type="submit" class="btn btn-primary">Cancel</button> -->
+									<button id="send" type="submit" class="btn btn-success">Input</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
